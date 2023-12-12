2ac0eca738b7261d3520442c467182a1c782f731
khayo254 <khayo1998@gmail.com>
2023-12-12 23:07:05 +0300

    pycodestyle corrections

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
-Contains the FileStorage class
+FileStorage class
 """
 
 import json
+import os
+
+
 from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
@@ -12,16 +15,23 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {
+    "Amenity": Amenity,
+    "BaseModel": BaseModel,
+    "User": User,
+    "City": City,
+    "Place": Place,
+    "Review": Review,
+    "State": State
+    }
 
 
 class FileStorage:
     """serializes instances to a JSON file & deserializes back to instances"""
 
     # string - path to the JSON file
-    __file_path = "file.json"
-    # dictionary - empty but will store all objects by <class name>.id
+    __file_path = os.path.join(os.path.dirname(__file__), "file.json")
+    # empty dictionary to store all objects by <class name>.id
     __objects = {}
 
     def all(self):
@@ -31,7 +41,7 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         if obj is not None:
-            key = obj.__class__.__name__ + "." + obj.id
+            key = f"{obj.__class__.__name__}.{obj.id}"
             self.__objects[key] = obj
 
     def save(self):
@@ -46,8 +56,17 @@ class FileStorage:
         """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, 'r') as f:
-                jo = json.load(f)
-            for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+                json_data = json.load(f)
+            for key, value in json_data.items():
+                class_name, obj_id = key.split('.')
+                if class_name in classes:
+                    cls = classes[class_name]
+                    self.__objects[key] = cls(**value)
+                else:
+                    print(f"Unknown class '{class_name}' encountered in JSON.")
+        except FileNotFoundError:
             pass
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in {self.__file_path}: {str(e)}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {str(e)}")
